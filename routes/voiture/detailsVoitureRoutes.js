0c04e53eea305b3017d8ce8176e4c1cfca0e0b56
lauraophelie <lauraophelie1@gmail.com>
2025-03-25 01:48:54 +0300

    upload images multer

--- routes/voiture/detailsVoitureRoutes.js
+++ routes/voiture/detailsVoitureRoutes.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../../config/storage');
+const DetailsVoiture = require('../../models/voiture/DetailsVoiture');
+
+router.post('/:voitureId', upload.array('images', 5), async (req, res) => {
+    try {
+        const { voitureId } = req.params;
+        const imagePaths = req.files.map(file => file.path);
+        const detailsVoiture = new DetailsVoiture({
+            voitureId: voitureId,
+            remarques: body.remarques,
+            images: imagePaths
+        });
+        await detailsVoiture.save();
+        res.status(201).json(detailsVoiture);
+    } catch(error) {
+        res.status(400).json({ message: error.message });
+    }
+});
 
 module.exports = router;
